1ad2a7f8a018635f9ddb207d205820acc239883e
rarezhang <catezhang@qq.com>
2017-02-04 03:17:41 +0000

    add read_column()

--- src/ReadTextFile.py
+++ src/ReadTextFile.py
@@ -4,21 +4,31 @@ class ReadTextFile:
     output file of MongoDB or mysql
     """
 
-    def __init__(self, path, read_label=True):
+    def __init__(self, path, sep=','):
         """
         read file with two columns: label, tweet
         :param path: path to the tweet file
-        :param readLabel: read Label or not
         """
         self.path = path
-        self.read_label = read_label
+        self.sep = sep
 
-    def read(self, sep=','):
+    def read(self):
         with open(self.path, mode='r', encoding='utf-8', errors='ignore') as f:
             for line in f:
-                line = line.split(sep)
-                label, tweet = line[0], line[1]
-                if self.read_label:
-                    yield label, tweet
-                else:
-                    yield tweet
+                line = line.split(self.sep)
+                # label, tweet = line[0], line[1]
+                # if self.read_label:
+                #     yield label, tweet
+                # else:
+                #     yield tweet
+                yield line
+
+    def read_column(self, col):
+        """
+
+        :param col: int, index of 'line' list
+        :return:
+        """
+        assert isinstance(col, int) and 0 <= col, 'check index'
+        for line in self.read():
+            yield line[col]
