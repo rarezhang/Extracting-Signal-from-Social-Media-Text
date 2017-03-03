524005ca8b86d8414a30b8829da4c047fecdf680
rarezhang <catezhang@qq.com>
2017-03-03 05:56:58 +0000

    examples

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,16 +1,21 @@
+from utils import push_var
+dataset = 'asthma'
+push_var(dataset)
+
+
 from ReadTextFile import ReadTextFile
-from Clean import Clean
+# from Clean import Clean
 from FeatureEng import FeatureEng
 from Classification import Classification
 
+
 # main
 general_text = '../data/training/'
-general_feature = '../data/feature/'
+# general_feature = '../data/feature/'
 general_output = '../data/result/'
 
-dataset = 'asthma'
 
-# ReadTextFile
+# training data path
 if dataset == 'ecig':
     path = f'{general_text}ecig_training_topic.csv'
 elif dataset == 'asthma':
@@ -20,28 +25,24 @@ else:
 
 
 rtf = ReadTextFile(path, sep='||')
-
 text = rtf.read_column(1)  # generator, column 1: text
-# for t in text: print(t)
 
 #######################################################################
 
-# Clean
-# clean
-clean = Clean(text)
-clean_text = clean.clean()
-
-path_feature = general_feature + dataset
-
-
+# # Clean
+# # clean
+# clean = Clean(text)
+# clean_text = clean.clean()
 fea = FeatureEng(text)
-fea.make_feature(path_feature, remake=False)  # todo: remake=F if not necessary
-X = fea.combine_feature(path_feature)
+# fea.make_feature(path_feature, remake=False)  # todo: remake=F if not necessary
+X = fea.feature_engineering(feature_type='ALL')
 print('-----------------------', X.shape)
 y = rtf.read_column(0)
 y = list(y)
 print('-----------------------', len(y))
-
+print(type(X))
+# X = X.toarray()
+# print(type(X))
 
 
 #######################################################################
@@ -57,13 +58,14 @@ from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.neural_network import MLPClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
 classifiers = (
     Perceptron(),
 )
 
 output_directory = general_output + dataset
-clf = Classification(output_directory, X, y)
-clf.classifier_comparison_cross_validation(classifiers, k=5)
+clf = Classification(output_directory, X, y, normalize=True)
+clf.classifier_comparison_cross_validation(classifiers, k=3)
 
 
 #######################################################################
